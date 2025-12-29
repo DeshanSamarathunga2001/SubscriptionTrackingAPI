b1475c7fa88c1a04edd85d29e6e89df7d9dec7d9
Deshan Samarathunga <deshansamarathunga13@gmail.com>
2025-12-29 18:07:27 +0530

    DataBase Connection Done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
@@ -17,8 +18,11 @@ app.get('/',(req , res) =>{
     res.send('Hello World');
 })
 
-app.listen(PORT,() =>{
+app.listen(PORT,async() =>{
     console.log(`API RUNNING PORT on http://localhost:${PORT}`);
+    await connectToDatabase();
 })
+
+
 
 export default app;
